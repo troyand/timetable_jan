60e1d314343abd512ad7d980fdd7b65383845b42
troyand <troyanovsky@gmail.com>
2012-01-15 20:38:32 +0200

    bootstrapping script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+rm timetable_jan/db.sqlite
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_1.csv -y 1 -c 6.030401 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_2.csv -y 2 -c 6.030401 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_3.csv -y 3 -c 6.030401 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_4.csv -y 4 -c 6.030401 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_5.csv -y 5 -c 6.030401 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_6.csv -y 6 -c 6.030401 -i
