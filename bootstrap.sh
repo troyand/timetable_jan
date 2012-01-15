6c16dd6fa52d84723716216ecd38eaed40498cbb
troyand <troyanovsky@gmail.com>
2012-01-15 20:38:46 +0200

    major type in index

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,28 @@
+#!/usr/bin/bash
 rm timetable_jan/db.sqlite
+python manage.py syncdb
+
+# FCSS
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_cs_1.csv -y 1 -c 6.050103 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_cs_2.csv -y 2 -c 6.050103 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_cs_3.csv -y 3 -c 6.050103 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_cs_4.csv -y 4 -c 6.050103 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_math_1.csv -y 1 -c 6.040301 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_math_2.csv -y 2 -c 6.040301 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_math_3.csv -y 3 -c 6.040301 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_math_4.csv -y 4 -c 6.040301 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_cs_5.csv -y 1 -c 7.05010101 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_iust_1.csv -y 1 -c 8.05010101 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_ispr_1.csv -y 1 -c 8.04030302 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_pzas_1.csv -y 1 -c 8.05010203 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_iust_2.csv -y 2 -c 8.05010101 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_ispr_2.csv -y 2 -c 8.04030302 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_pzas_2.csv -y 2 -c 8.05010203 -i
+
+# FLS
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_1.csv -y 1 -c 6.030401 -i
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_2.csv -y 2 -c 6.030401 -i
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_3.csv -y 3 -c 6.030401 -i
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_4.csv -y 4 -c 6.030401 -i
-python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_5.csv -y 5 -c 6.030401 -i
-python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_6.csv -y 6 -c 6.030401 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_5.csv -y 1 -c 7.03040201 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_fls_6.csv -y 1 -c 8.03040201 -i
