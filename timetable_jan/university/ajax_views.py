a8d337b08875a9fee94da7444957c9e715733bb2
troyand <troyanovsky@gmail.com>
2012-01-26 12:35:43 +0200

    generic autocomplete_response with case sensitivity workaround

--- timetable_jan/university/ajax_views.py
+++ timetable_jan/university/ajax_views.py
@@ -7,8 +7,21 @@ import json
 
 class AjaxAutocompleteMixin(object):
     def autocomplete_response(self, query):
-        '''implementation should return json response'''
-        raise NotImplementedError
+        '''implementation should return json response
+        in a more effective way (e.g. via icontains)'''
+        query_upper = query.upper()
+        object_unicode_pk_pairs = [
+                (unicode(o), o.pk) for o in self.model.objects.select_related().all()]
+        objects = filter(
+                lambda o: o[0].upper().startswith(query_upper), object_unicode_pk_pairs)
+        json_response = {
+                'query': query,
+                'suggestions': [u for u, pk in objects],
+                'data': [pk for u, pk in objects],
+                }
+        return HttpResponse(
+                json.dumps(json_response)
+                )
     def get(self, request):
         try:
             return self.autocomplete_response(request.GET['query'])
@@ -18,7 +31,7 @@ class AjaxAutocompleteMixin(object):
 
 class RoomAutocompleteView(AjaxAutocompleteMixin, BaseListView):
     model = Room
-    def autocomplete_response(self, query):
+    def autocomplete_response_(self, query):
         room_name_pk_pairs = [(unicode(r), r.pk) for r in Room.objects.all()]
         rooms = filter(lambda x: x[0].startswith(query), room_name_pk_pairs)
         json_response = {
@@ -32,7 +45,7 @@ class RoomAutocompleteView(AjaxAutocompleteMixin, BaseListView):
 
 class LecturerAutocompleteView(AjaxAutocompleteMixin, BaseListView):
     model = Lecturer
-    def autocomplete_response(self, query):
+    def autocomplete_response_(self, query):
         lecturers = [(l.full_name, l.pk) for l in Lecturer.objects.filter(full_name__icontains=query)]
         json_response = {
                 'query': query,
