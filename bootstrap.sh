e8248db31e1a7cd52fa57b05dbdba6c02ee73b50
troyand <troyanovsky@gmail.com>
2012-02-05 13:21:31 +0200

    socrob 3

--- bootstrap.sh
+++ bootstrap.sh
@@ -80,6 +80,7 @@ python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_socrob_
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_politology_2.csv -y 2 -c 6.030104 -i
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_politology_3.csv -y 3 -c 6.030104 -i
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_sociology_3.csv -y 3 -c 6.030101 -i
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_vesna_socrob_3.csv -y 3 -c 6.130102 -i
 
 # foreign courses
 python add_courses.py -f 6.030101 -y 1 -t 6.030104 -z 1 -n "Логіка"
