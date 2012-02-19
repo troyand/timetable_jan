008a461ff076afd8c82faaec6d53ee4427d5f4d2
troyand <troyanovsky@gmail.com>
2012-02-19 22:39:01 +0200

    enroll and quit tests

--- timetable_jan/university/tests.py
+++ timetable_jan/university/tests.py
@@ -6,6 +6,8 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.db import IntegrityError
+from timetable_jan.university.models import *
 
 
 class SimpleTest(TestCase):
@@ -14,3 +16,156 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class TestStudentEnrollment(TestCase):
+    fixtures = ['test_timetable_dataset.xml']
+
+    def setUp(self):
+        # save group objects for easy retrieval in test cases
+        self.phy_0, self.mat_0, self.mat_1, self.mat_2 = Group.objects.select_related().all()
+        self.student = Student.objects.create(full_name='John Doe')
+
+    def test_lesson_enrollment_phy_lecture(self):
+        """Test that a student can enroll to a separate lesson"""
+        lesson = self.phy_0.lesson_set.all()[0]
+        self.student.enroll(lesson)
+        self.assertEqual(
+                set([lesson]),
+                set(self.student.lessons())
+                )
+
+    def test_group_enrollment_phy_lectures(self):
+        """Test that individual StudentLessonSubscription objects are created when
+        a student enrolls to a group (should be triggerred by signal)"""
+        self.student.enroll(self.phy_0)
+        self.assertEqual(
+                set(self.phy_0.lesson_set.all()),
+                set(self.student.lessons())
+                )
+
+    def test_group_enrollment_multiple_groups(self):
+        """Test to verify that a student may enroll to multiple groups"""
+        groups = [self.phy_0, self.mat_0, self.mat_1]
+        for group in groups:
+            self.student.enroll(group)
+        self.assertEqual(
+                set([l for g in groups for l in g.lesson_set.all()]),
+                set(self.student.lessons())
+                )
+
+    def test_group_and_lesson_enrollment_mat(self):
+        """Test to verify that a student may enroll both to group and
+        single lesson"""
+        self.student.enroll(self.mat_0)
+        self.student.enroll(self.mat_1.lesson_set.all()[0])
+        self.assertEqual(
+                set(list(self.mat_0.lesson_set.all()) + [self.mat_1.lesson_set.all()[0]]),
+                set(self.student.lessons())
+                )
+
+    def test_group_enrollment_double_save(self):
+        """Test that saving existing StudentGroupMembership does not
+        trigger unnecessary processing"""
+        self.student.enroll(self.phy_0)
+        sgm = StudentGroupMembership.objects.get()
+        # here no exception should be raised
+        sgm.save()
+        self.assertEqual(
+                set(self.phy_0.lesson_set.all()),
+                set(self.student.lessons())
+                )
+
+    def test_lesson_enrollment_twice(self):
+        """Test that enrolling twice to the same lesson raises
+        IntegrityError"""
+        lesson = self.phy_0.lesson_set.all()[0]
+        self.student.enroll(lesson)
+        self.assertRaises(
+                IntegrityError,
+                self.student.enroll,
+                lesson
+                )
+
+    def test_group_enrollment_twice(self):
+        """Test that enrolling twice to the same group raises
+        IntegrityError"""
+        self.student.enroll(self.phy_0)
+        self.assertRaises(
+                IntegrityError,
+                self.student.enroll,
+                self.phy_0
+                )
+
+    def test_lesson_group_enrollment_override(self):
+        """Test that enrolling for individual lesson
+        that was in group series breaks the group link"""
+        self.student.enroll(self.phy_0)
+        self.student.enroll(self.phy_0.lesson_set.all()[0])
+        self.assertEqual(
+                set(self.phy_0.lesson_set.all()),
+                set(self.student.lessons())
+                )
+
+    def test_lesson_quitting_phy_lecture(self):
+        """Test that a student can quit a separate lesson"""
+        lesson = self.phy_0.lesson_set.all()[0]
+        self.student.enroll(lesson)
+        self.assertEqual(
+                set([lesson]),
+                set(self.student.lessons())
+                )
+        self.student.quit(lesson)
+        self.assertEqual(
+                set(),
+                set(self.student.lessons())
+                )
+
+    def test_group_quitting_phy_lectures(self):
+        """Test that individual StudentLessonSubscription objects are deleted when
+        a student quits a group (should be triggerred by cascade DB delete)"""
+        self.student.enroll(self.phy_0)
+        self.assertEqual(
+                set(self.phy_0.lesson_set.all()),
+                set(self.student.lessons())
+                )
+        self.student.quit(self.phy_0)
+        self.assertEqual(
+                set(),
+                set(self.student.lessons())
+                )
+
+    def test_group_and_lesson_quitting_mat(self):
+        """Test to verify that a student may quit both a group and
+        a single lesson"""
+        self.student.enroll(self.mat_0)
+        self.student.enroll(self.mat_1.lesson_set.all()[0])
+        self.assertEqual(
+                set(list(self.mat_0.lesson_set.all()) + [self.mat_1.lesson_set.all()[0]]),
+                set(self.student.lessons())
+                )
+        self.student.quit(self.mat_0)
+        self.assertEqual(
+                set([self.mat_1.lesson_set.all()[0]]),
+                set(self.student.lessons())
+                )
+        self.student.quit(self.mat_1.lesson_set.all()[0])
+        self.assertEqual(
+                set(),
+                set(self.student.lessons())
+                )
+
+    def test_lesson_group_quitting_override(self):
+        """Test that enrolling for individual lesson
+        that was in group series breaks the group link
+        and quitting group does not delete the
+        overriden lesson"""
+        self.student.enroll(self.phy_0)
+        lesson = self.phy_0.lesson_set.all()[0]
+        self.student.enroll(lesson)
+        self.student.quit(self.phy_0)
+        self.assertEqual(
+                set([lesson]),
+                set(self.student.lessons())
+                )
+
