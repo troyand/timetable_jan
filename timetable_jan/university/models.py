da7c2c53fc25c05163d18584b91c50aa6a784614
troyand <troyanovsky@gmail.com>
2012-02-25 11:40:03 +0200

    try to remove abstract person

--- timetable_jan/university/models.py
+++ timetable_jan/university/models.py
@@ -202,8 +202,8 @@ class Person(models.Model):
     """Represents generic person"""
     full_name = models.CharField(max_length=255)
     
-    class Meta:
-        abstract = True
+    #class Meta:
+    #    abstract = True
 
     def short_name(self):
         parts = self.full_name.split(' ')
