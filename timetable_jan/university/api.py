4fae97c1030b03c2481a388602bf597bbeeb9ce7
gsnewmark <gsnewmark@meta.ua>
2012-03-01 00:00:35 +0200

    add additional resources

--- timetable_jan/university/api.py
+++ timetable_jan/university/api.py
@@ -59,10 +59,13 @@ class ModelResource(ModelResource):
 class UniversityResource(ModelResource):
     terms = fields.ToManyField(
         'timetable_jan.university.api.AcademicTermResource',
-        "terms", "university", null=True)
+        'terms', 'university', null=True)
     buildings = fields.ToManyField(
         'timetable_jan.university.api.BuildingResource',
-        "buildings", "university", null=True)
+        'buildings', 'university', null=True)
+    faculties = fields.ToManyField(
+        'timetable_jan.university.api.FacultyResource',
+        'faculties', 'university', null=True)
 
     class Meta:
         queryset = University.objects.all()
@@ -72,6 +75,9 @@ class UniversityResource(ModelResource):
 
 class AcademicTermResource(ModelResource):
     university = fields.ForeignKey(UniversityResource, 'university')
+    courses = fields.ToManyField(
+        'timetable_jan.university.api.CourseResource',
+        'courses', 'academic_term', null=True)
 
     class Meta:
         queryset = AcademicTerm.objects.all()
@@ -79,7 +85,7 @@ class AcademicTermResource(ModelResource):
         allowed_methods = ['get']
 
         filtering = {
-            "university": ('exact',),
+            'university': ('exact',),
             }
 
 
@@ -110,7 +116,86 @@ class RoomResource(ModelResource):
         filtering = {
             'building': ('exact',),
             }
+
+
+class FacultyResource(ModelResource):
+    university = fields.ForeignKey(UniversityResource, 'university')
+    departments = fields.ToManyField(
+        'timetable_jan.university.api.DepartmentResource',
+        'departments', 'faculty', null=True)
+    majors = fields.ToManyField(
+        'timetable_jan.university.api.MajorResource',
+        'majors', 'faculty', null=True)
+
+    class Meta:
+        queryset = Faculty.objects.all()
+        resource_name = 'faculties'
+        allowed_methods = ['get']
+
+        filtering = {
+            'university': ('exact',),
+            }
+
         
+class DepartmentResource(ModelResource):
+    faculty = fields.ForeignKey(FacultyResource, 'faculty')
+    disciplines = fields.ToManyField(
+        'timetable_jan.university.api.DisciplineResource',
+        'disciplines', 'department', null=True)
+
+    class Meta:
+        queryset = Department.objects.all()
+        resource_name = 'departments'
+        allowed_methods = ['get']
+
+        filtering = {
+            'faculty': ('exact',),
+            }
+
+
+class MajorResource(ModelResource):
+    faculty = fields.ForeignKey(FacultyResource, 'faculty')
+
+    class Meta:
+        queryset = Major.objects.all()
+        resource_name = 'majors'
+        allowed_methods = ['get']
+
+        filtering = {
+            'faculty': ('exact',),
+            }
+
+
+class DisciplineResource(ModelResource):
+    department = fields.ForeignKey(DepartmentResource, 'department')
+    courses = fields.ToManyField(
+        'timetable_jan.university.api.CourseResource',
+        'courses', 'discipline', null=True)
+
+    class Meta:
+        queryset = Discipline.objects.all()
+        resource_name = 'disciplines'
+        allowed_methods = ['get']
+
+        filtering = {
+            'department': ('exact',),
+            }
+
+
+class CourseResource(ModelResource):
+    discipline = fields.ForeignKey(DisciplineResource, 'discipline')
+    term = fields.ForeignKey(AcademicTermResource, 'academic_term')
+    
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        allowed_methods = ['get']
+
+        filtering = {
+            'discipline': ('exact',),
+            'academic_term': ('exact',),
+            }
+
 
 #------------------------ API links ----------------------------------
 v1_api = Api(api_name='v1')
@@ -118,6 +203,11 @@ v1_api.register(UniversityResource())
 v1_api.register(AcademicTermResource())
 v1_api.register(BuildingResource())
 v1_api.register(RoomResource())
+v1_api.register(FacultyResource())
+v1_api.register(DepartmentResource())
+v1_api.register(MajorResource())
+v1_api.register(DisciplineResource())
+v1_api.register(CourseResource())
 
 urls = patterns('',
     (r'^', include(v1_api.urls)),
