edafe7e5d59b55740b789ce752cf7363fccb5cf7
troyand <troyanovsky@gmail.com>
2012-04-01 18:29:59 +0300

    summer academic term pk fix

--- summer_continuation.py
+++ summer_continuation.py
@@ -7,7 +7,7 @@ setup_environ(settings)
 
 
 from timetable_jan.university.models import *
-print AcademicTerm.objects.get_or_create(
+academic_term, created = AcademicTerm.objects.get_or_create(
         university=University.objects.get(abbr=u'НаУКМА'),
         year=2011,
         kind=u'семестр',
@@ -17,4 +17,5 @@ print AcademicTerm.objects.get_or_create(
         start_date='2012-04-15',
         exams_start_date='2012-05-27',
         exams_end_date='2012-06-07',
-        ).pk
+        )
+print academic_term.pk
