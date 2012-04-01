0fd1bca5eded98076ca72e11b3b1b7e015522a8e
troyand <troyanovsky@gmail.com>
2012-04-01 19:52:26 +0300

    law 4

--- summer_continuation.sh
+++ summer_continuation.sh
@@ -7,4 +7,5 @@ echo $academic_term_id
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_fls_1.csv -y 1 -c 6.030401 -i -t $academic_term_id
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_fls_2.csv -y 2 -c 6.030401 -i -t $academic_term_id
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_fls_3.csv -y 3 -c 6.030401 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_fls_4.csv -y 4 -c 6.030401 -i -t $academic_term_id
 
