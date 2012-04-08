2f875a1ab2845eda087afd4bd20b299022985d2e
troyand <troyanovsky@gmail.com>
2012-04-08 15:52:47 +0300

    validation for feedback form

--- timetable_jan/university/forms.py
+++ timetable_jan/university/forms.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-from django.forms import ModelForm, Form, CharField, Textarea
+from django.forms import ModelForm, Form, CharField, Textarea, ValidationError
 from timetable_jan.university.models import *
 
 class UserForm(ModelForm):
@@ -23,18 +23,27 @@ class FeedbackForm(Form):
     liked = CharField(
             widget=Textarea,
             max_length=5000,
-            required=False,
+            required=True,
             label=u'Що найбільше сподобалось'
             )
     disliked = CharField(
             widget=Textarea,
             max_length=5000,
-            required=False,
+            required=True,
             label=u'Що найбільше не сподобалось'
             )
     wouldliked = CharField(
             widget=Textarea,
             max_length=5000,
-            required=False,
+            required=True,
             label=u'Чого найбільше не вистачало'
             )
+    def ensure_min_width(self, data):
+        if len(data) < 10:
+            raise ValidationError(u'Ви були не надто багатослівними')
+    def clean_liked(self):
+        return self.ensure_min_width(self.cleaned_data['liked'])
+    def clean_disliked(self):
+        return self.ensure_min_width(self.cleaned_data['disliked'])
+    def clean_wouldliked(self):
+        return self.ensure_min_width(self.cleaned_data['wouldliked'])
