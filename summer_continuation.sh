10b67a1b122a3901d5a33efe2275ff88c0e024cd
troyand <troyanovsky@gmail.com>
2012-04-18 17:06:00 +0300

    fssst in import script

--- summer_continuation.sh
+++ summer_continuation.sh
@@ -9,3 +9,12 @@ python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_fls_2.cs
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_fls_3.csv -y 3 -c 6.030401 -i -t $academic_term_id
 python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_fls_4.csv -y 4 -c 6.030401 -i -t $academic_term_id
 
+# FSSST
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_sociology_1.csv -y 1 -c 6.030101 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_politology_1.csv -y 1 -c 6.030104 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_sociology_2.csv -y 2 -c 6.030101 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_socrob_2.csv -y 2 -c 6.130102 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_politology_2.csv -y 2 -c 6.030104 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_sociology_3.csv -y 3 -c 6.030101 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_socrob_3.csv -y 3 -c 6.130102 -i -t $academic_term_id
+python import_timetable.py -f timetable_jan/unified_docs/2011_2012_lito_politology_3.csv -y 3 -c 6.030104 -i -t $academic_term_id
