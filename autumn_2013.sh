5e518953b04be6ad354f5d80f0147d3286708bbf
Ivan Kryvoruchko <gildraug@gmail.com>
2013-09-05 00:42:17 +0300

    Add timetables for BS-1 math and cs.

--- autumn_2013.sh
+++ autumn_2013.sh
@@ -10,3 +10,11 @@ python import_timetable.py -f timetable/unified_docs/2013_2014_osin_fls_3.csv -y
 python import_timetable.py -f timetable/unified_docs/2013_2014_osin_fls_4.csv -y 4 -c 6.030401 -i -t $academic_term_id
 python import_timetable.py -f timetable/unified_docs/2013_2014_osin_fls_5.csv -y 1 -c 7.03040201 -i -t $academic_term_id
 python import_timetable.py -f timetable/unified_docs/2013_2014_osin_fls_6.csv -y 1 -c 8.03040201 -i -t $academic_term_id
+
+# FI
+python import_timetable.py -f timetable/unified_docs/2013_2014_osin_cs_1.csv -y 1 -c 6.050103 -i -t $academic_term_id
+
+python import_timetable.py -f timetable/unified_docs/2012_2013_osin_math_1.csv -y 1 -c 6.040301 -i -t $academic_term_id
+python add_courses.py -f 6.050103 -y 1 -t 6.040301 -z 1 -n "Англійська мова" -a $academic_term_id
+python add_courses.py -f 6.050103 -y 1 -t 6.040301 -z 1 -n "Застосування принципів Болонського процесу в НаУКМА" -a $academic_term_id
+python add_courses.py -f 6.050103 -y 1 -t 6.040301 -z 1 -n "Українська мова" -a $academic_term_id
