f17bd4c9aeb9ebb6dfe8763d4a88a866207ea5f7
Ivan Kryvoruchko <gildraug@gmail.com>
2013-09-08 16:03:21 +0300

    Add additional timetables for math, cs BS.

--- autumn_2013.sh
+++ autumn_2013.sh
@@ -13,8 +13,21 @@ python import_timetable.py -f timetable/unified_docs/2013_2014_osin_fls_6.csv -y
 
 # FI
 python import_timetable.py -f timetable/unified_docs/2013_2014_osin_cs_1.csv -y 1 -c 6.050103 -i -t $academic_term_id
-
 python import_timetable.py -f timetable/unified_docs/2013_2014_osin_math_1.csv -y 1 -c 6.040301 -i -t $academic_term_id
 python add_courses.py -f 6.050103 -y 1 -t 6.040301 -z 1 -n "Англійська мова" -a $academic_term_id
 python add_courses.py -f 6.050103 -y 1 -t 6.040301 -z 1 -n "Застосування принципів Болонського процесу в НаУКМА" -a $academic_term_id
 python add_courses.py -f 6.050103 -y 1 -t 6.040301 -z 1 -n "Українська мова" -a $academic_term_id
+
+python import_timetable.py -f timetable/unified_docs/2013_2014_osin_cs_2.csv -y 2 -c 6.050103 -i -t $academic_term_id
+python import_timetable.py -f timetable/unified_docs/2013_2014_osin_math_2.csv -y 2 -c 6.040301 -i -t $academic_term_id
+python add_courses.py -f 6.050103 -y 2 -t 6.040301 -z 2 -n "Англійська мова" -a $academic_term_id
+python add_courses.py -f 6.050103 -y 2 -t 6.040301 -z 2 -n "Українська мова (за професійним спрямуванням)" -a $academic_term_id
+python add_courses.py -f 6.050103 -y 2 -t 6.040301 -z 2 -n "Процедурне програмування (на базі Сі/Сі++)" -a $academic_term_id
+
+python import_timetable.py -f timetable/unified_docs/2013_2014_osin_cs_3.csv -y 3 -c 6.050103 -i -t $academic_term_id
+python import_timetable.py -f timetable/unified_docs/2013_2014_osin_math_3.csv -y 3 -c 6.040301 -i -t $academic_term_id
+python add_courses.py -f 6.050103 -y 3 -t 6.040301 -z 3 -n "Безпека життєдіяльності" -a $academic_term_id
+python add_courses.py -f 6.050103 -y 3 -t 6.040301 -z 3 -n "Філософія" -a $academic_term_id
+
+python import_timetable.py -f timetable/unified_docs/2013_2014_osin_cs_4.csv -y 4 -c 6.050103 -i -t $academic_term_id
+python import_timetable.py -f timetable/unified_docs/2013_2014_osin_math_4.csv -y 4 -c 6.040301 -i -t $academic_term_id
